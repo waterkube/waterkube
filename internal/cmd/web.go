541f7ab3448651124b0c6ff42d42f07a467e261d
Peter Takacs <peter.takacs@koodilab.com>
2021-11-26 08:06:42 +0100

    Added the web command

---
+++ internal/cmd/web.go
@@ -0,0 +1,10 @@
+package cmd
+
+import "github.com/petaki/support-go/cli"
+
+// WebServe command.
+func WebServe(group *cli.Group, command *cli.Command, arguments []string) int {
+	// TODO: Run the server.
+
+	return cli.Success
+}
--- main.go
+++ main.go
@@ -1,10 +1,26 @@
 package main
 
-import "github.com/petaki/support-go/cli"
+import (
+	"github.com/petaki/support-go/cli"
+	"github.com/petaki/waterkube/internal/cmd"
+)
 
 func main() {
 	(&cli.App{
 		Name:    "Waterkube",
 		Version: "1.0.0",
+		Groups: []*cli.Group{
+			{
+				Name:  "web",
+				Usage: "Web commands",
+				Commands: []*cli.Command{
+					{
+						Name:       "serve",
+						Usage:      "Serve the app",
+						HandleFunc: cmd.WebServe,
+					},
+				},
+			},
+		},
 	}).Execute()
 }
