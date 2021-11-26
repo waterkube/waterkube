df5c58f5244c08da1eae8155d7870f8cb3269ea4
Peter Takacs <peter.takacs@koodilab.com>
2021-11-26 08:31:46 +0100

    Added command skeletons

--- main.go
+++ main.go
@@ -11,6 +11,44 @@ func main() {
 		Version: "1.0.0",
 		Groups: []*cli.Group{
 			{
+				Name:  "artifact",
+				Usage: "Artifact commands",
+				Commands: []*cli.Command{
+					{
+						Name:  "sell",
+						Usage: "Sell an artifact",
+					},
+				},
+			},
+			{
+				Name:  "diver",
+				Usage: "Diver commands",
+				Commands: []*cli.Command{
+					{
+						Name:  "explore",
+						Usage: "Explore the coordinate",
+					},
+					{
+						Name:  "hire",
+						Usage: "Hire a diver",
+					},
+				},
+			},
+			{
+				Name:  "submarine",
+				Usage: "Submarine commands",
+				Commands: []*cli.Command{
+					{
+						Name:  "explore",
+						Usage: "Explore the coordinate",
+					},
+					{
+						Name:  "buy",
+						Usage: "Buy a submarine",
+					},
+				},
+			},
+			{
 				Name:  "web",
 				Usage: "Web commands",
 				Commands: []*cli.Command{
