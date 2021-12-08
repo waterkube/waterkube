d19b3d69684a390492342357ea5d0a4da2893d4f
Peter Takacs <peter.takacs@koodilab.com>
2021-12-08 07:24:56 +0100

    Updated eslint

--- resources/js/pages/game/Index.vue
+++ resources/js/pages/game/Index.vue
@@ -1,10 +1,10 @@
 <template>
     <inertia-head title="Deep Sea" />
-    <div class="flex pt-72">
-        <div class="inline-grid gap-1 lg:gap-2 grid-cols-10 mx-auto">
+    <div class="flex py-72">
+        <div class="inline-grid gap-1 sm:gap-1.5 lg:gap-2 grid-cols-10 mx-auto">
             <div v-for="item in 100"
                  :key="item"
-                 class="bg-sky-500 w-4 h-4 sm:w-8 sm:h-8 lg:w-16 lg:h-16"></div>
+                 class="bg-sky-500 w-6 h-6 sm:w-12 sm:h-12 lg:w-16 lg:h-16"></div>
         </div>
     </div>
 </template>
