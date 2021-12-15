1174a7da05e7ae55988a8bde14466a138f97cb39
Peter Takacs <peter.takacs@koodilab.com>
2021-12-15 01:14:43 +0100

    CS fix

--- internal/models/exploration.go
+++ internal/models/exploration.go
@@ -7,6 +7,7 @@ const (
 	ExplorationTime = 10 * time.Second
 )
 
+// Exploration type.
 type Exploration struct {
 	Grid     string   `json:"grid" redis:"grid"`
 	GridType GridType `json:"grid_type" redis:"grid_type"`
