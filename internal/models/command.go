efb60c10bdd4848ecf22e8821d5a9ffeb125ad53
Peter Takacs <peter.takacs@koodilab.com>
2021-12-17 07:37:43 +0100

    Enhanced the command model

--- internal/models/command.go
+++ internal/models/command.go
@@ -3,7 +3,9 @@ package models
 // Command type.
 type Command struct {
 	ID        int64     `json:"id" redis:"id"`
-	Value     string    `json:"value" redis:"value"`
+	GroupName string    `json:"group_name" redis:"group_name"`
+	Name      string    `json:"name" redis:"name"`
+	Arguments []string  `json:"arguments" redis:"arguments"`
 	CreatedAt Timestamp `json:"created_at" redis:"created_at"`
 }
 
