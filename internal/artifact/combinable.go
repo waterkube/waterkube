a989ee8e277cdc47a82bf0094b04bd5b2f9fe67a
Peter Takacs <peter.takacs@koodilab.com>
2022-04-06 02:13:26 +0200

    Added combinable items

--- internal/artifact/combinable.go
+++ internal/artifact/combinable.go
@@ -2,23 +2,45 @@ package artifact
 
 // Combinable type.
 type Combinable struct {
-	Price int
-	Pair  string
+	Price  int
+	Pair   string
+	Result string
 }
 
 // ShallowCombinable list.
 var ShallowCombinable = map[string]*Combinable{
-	"Chest": {
-		Price: 400,
-		Pair:  "Key",
-	},
 	"Key": {
-		Price: 50,
-		Pair:  "Chest",
+		Price:  2,
+		Pair:   "Chest",
+		Result: "Treasure",
+	},
+	"Chest": {
+		Price:  2,
+		Pair:   "Key",
+		Result: "Treasure",
 	},
 }
 
 // DeepCombinable list.
 var DeepCombinable = map[string]*Combinable{
-	// TODO
+	"Code": {
+		Price:  3,
+		Pair:   "Safe",
+		Result: "Gold",
+	},
+	"Safe": {
+		Price:  3,
+		Pair:   "Code",
+		Result: "Gold",
+	},
+	"Crowbar": {
+		Price:  4,
+		Pair:   "Container",
+		Result: "Automobile",
+	},
+	"Container": {
+		Price:  4,
+		Pair:   "Crowbar",
+		Result: "Automobile",
+	},
 }
